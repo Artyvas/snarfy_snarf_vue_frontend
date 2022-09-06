42affbe93d11b171712371b6553fae2605593407
“Artyvas” <artemvasily@gmail.com>
2022-09-06 16:40:37 -0700

    Adds bookings index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>All Bookings</h1>
+    <div v-for="booking in bookings" v-bind:key="booking.id">
+      <h3>First Name: {{ booking.first_name }}</h3>
+      <h3>Last Name: {{ booking.last_name }}</h3>
+      <h3>Animal First Name: {{ booking.animal_first }}</h3>
+      <h3>Animal Last Name: {{ booking.animal_last }}</h3>
+      <h3>Animal Type: {{ booking.animal_type }}</h3>
+      <h3>Hours Requested: {{ booking.hours_rq }}</h3>
+      <h3>Date of Service: {{ booking.date_of_service }}</h3>
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      bookings: [],
+    };
+  },
+  created: function () {
+    this.indexBookings();
+  },
+  methods: {
+    indexBookings: function () {
+      axios.get("/api/bookings").then((response) => {
+        this.bookings = response.data;
+        console.log("All Bookings:", this.bookings);
+      });
+    },
   },
 };
 </script>
