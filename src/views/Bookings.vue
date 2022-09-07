cfb48b7a3669c6e5a95f74ec75c1da864fd351f7
“Artyvas” <artemvasily@gmail.com>
2022-09-06 17:15:26 -0700

    Bifurcates create booking and all bookings pages

---
+++ src/views/Bookings.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h1>All Bookings</h1>
+    <div v-for="booking in bookings" v-bind:key="booking.id">
+      <h3>First Name: {{ booking.first_name }}</h3>
+      <h3>Last Name: {{ booking.last_name }}</h3>
+      <h3>Animal First Name: {{ booking.animal_first }}</h3>
+      <h3>Animal Last Name: {{ booking.animal_last }}</h3>
+      <h3>Animal Type: {{ booking.animal_type }}</h3>
+      <h3>Hours Requested: {{ booking.hours_rq }}</h3>
+      <h3>Date of Service: {{ booking.date_of_service }}</h3>
+    </div>
+  </div>
+</template>
+<style></style>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      bookings: [],
+      newBookingFirstName: "",
+      newBookingLastName: "",
+      newBookingAnimalFirst: "",
+      newBookingAnimalLast: "",
+      newBookingAnimalType: "",
+      newBookingHoursRq: null,
+      newBookingDateOfService: "",
+    };
+  },
+  created: function () {
+    this.indexBookings();
+  },
+  methods: {
+    indexBookings: function () {
+      axios.get("/api/bookings").then((response) => {
+        this.bookings = response.data;
+        console.log("All Bookings:", this.bookings);
+      });
+    },
+  },
+};
+</script>
--- src/views/CreateBooking.vue
+++ src/views/CreateBooking.vue
@@ -18,16 +18,6 @@
       <input type="text" v-model="newBookingDateOfService" />
       <button v-on:click="createBooking()">Create</button>
     </div>
-    <h1>All Bookings</h1>
-    <div v-for="booking in bookings" v-bind:key="booking.id">
-      <h3>First Name: {{ booking.first_name }}</h3>
-      <h3>Last Name: {{ booking.last_name }}</h3>
-      <h3>Animal First Name: {{ booking.animal_first }}</h3>
-      <h3>Animal Last Name: {{ booking.animal_last }}</h3>
-      <h3>Animal Type: {{ booking.animal_type }}</h3>
-      <h3>Hours Requested: {{ booking.hours_rq }}</h3>
-      <h3>Date of Service: {{ booking.date_of_service }}</h3>
-    </div>
   </div>
 </template>
 
